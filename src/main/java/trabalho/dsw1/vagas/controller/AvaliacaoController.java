d7f638ae6c693a7914b266436ca68f8a5f239199
Larissa D Silva <larissadiasdasilv@gmail.com>
2025-06-24 00:39:22 -0300

    R6

--- src/main/java/trabalho/dsw1/vagas/controller/AvaliacaoController.java
+++ src/main/java/trabalho/dsw1/vagas/controller/AvaliacaoController.java
@@ -1,12 +1,19 @@
 package trabalho.dsw1.vagas.controller;
 
 import trabalho.dsw1.vagas.domain.Candidato;
+import trabalho.dsw1.vagas.domain.Empresa;
+import trabalho.dsw1.vagas.domain.Vaga;
 import trabalho.dsw1.vagas.dao.ICandidatoDAO;
+import trabalho.dsw1.vagas.dao.IVagaDAO;
+import trabalho.dsw1.vagas.repository.EmpresaRepository;
 import trabalho.dsw1.vagas.service.impl.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,7 +31,7 @@ public class AvaliacaoController {
     public String listarCandidatos(@PathVariable Long id, Model model) {
         List<Candidato> candidaturas = candidaturaRepository.findByVagaId(id);
         model.addAttribute("candidaturas", candidaturas);
-        return "avaliacao";
+        return "empresas/avaliacao";
     }
 
     @PostMapping("/candidatura/{id}/avaliar")
@@ -53,7 +60,28 @@ public class AvaliacaoController {
             .orElseThrow(() -> new RuntimeException("Candidatura não encontrada"));
 
         model.addAttribute("candidatura", candidatura);
-        return "avaliar-candidatura";
+        return "empresas/avaliar-candidatura";
+    }
+    
+    @Autowired
+    private EmpresaRepository empresaRepository;
+    @Autowired
+    private IVagaDAO vagaDAO;
+
+
+    @GetMapping("/lista-vagas-empresa")
+    public String listarVagasDaEmpresa(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+    String emailEmpresa = userDetails.getUsername(); // pega o email da empresa logada
+
+    // buscar a empresa pelo e-mail
+    Empresa empresa = empresaRepository.findByEmail(emailEmpresa).orElseThrow(() -> 
+    new RuntimeException("Empresa não encontrada"));
+
+    // busca as vagas da empresa
+    List<Vaga> vagas = vagaDAO.findByEmpresaId(empresa.getId());
+
+    model.addAttribute("vagas", vagas);
+    return "empresas/lista-vagas-empresa";
     }
 
 }
